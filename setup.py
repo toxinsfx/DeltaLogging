b0c885f08f5465a8522fada1662fc6726c659bfa
toxinsfx <143892201+toxinsfx@users.noreply.github.com>
2023-10-12 14:59:15 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DeltaLogging',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=["pystyle"],
+    author='toxinsfx',
+    description='Simple python logging library',
+    long_description='DeltaLogging is a simple logging library that has info,warning etc',
+    url='https://github.com/toxinsfx/deltalogging',
+    license='Open Source',
+)
